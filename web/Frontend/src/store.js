57388d31c6ccb23d32e0ac86aef5fe532460019d
wajiddaudtamboli <wajiddaudtamboli@users.noreply.github.com>
2025-12-09 18:04:10 +0530

    fix: Resolve dependency conflicts for Vercel deployment
    - Remove deprecated redux-devtools-extension package
    - Update store.js to use @redux-devtools/extension
    - Add --legacy-peer-deps flag to install commands
    - Fix npm peer dependency resolution issues

--- web/Frontend/src/store.js
+++ web/Frontend/src/store.js
@@ -1,6 +1,6 @@
 import { thunk } from 'redux-thunk'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';  // redux-persist for storing data in state
